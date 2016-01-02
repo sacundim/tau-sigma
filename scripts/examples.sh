a2b09a6ea81d5e71a2eb6c3b81d7e6dc91a23c00
Luis Casillas <luis@casillas.org>
2016-01-01 16:54:26 -0800

    Regenerate the examples.

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -6,20 +6,23 @@
 set -xe
 cd `dirname $0`/..
 
+VALUE=0.575
+TAUS=9
+
 for noise in wpm fpm wfm ffm rwfm
 do
-    tau-sigma noise --${noise} 0.95 \
+    tau-sigma noise --${noise} ${VALUE} \
         | tau-sigma chart \
                     --label "time error" \
                     --out images/${noise}_phase.svg
 
-    tau-sigma noise --${noise} 0.95 --frequency \
+    tau-sigma noise --${noise} ${VALUE} --frequency \
         | tau-sigma chart \
                     --label "frequency error" \
                     --out images/${noise}_frequency.svg
     
-    tau-sigma noise --${noise} 0.95 \
-        | tau-sigma adev --tau0 1 --max-tau 9 \
+    tau-sigma noise --${noise} ${VALUE} \
+        | tau-sigma adev --tau0 1 --max-tau ${TAUS} \
         | tau-sigma loglog \
                     --label "ADEV" \
                     --out images/${noise}_adev.svg
