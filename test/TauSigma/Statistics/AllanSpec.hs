09d1ba2b7d91372b702d5d503f4558edc105f866
Luis Casillas <luis@casillas.org>
2016-01-02 11:43:17 -0800

    Refactor of slope tests to make it cleaner to add new ones.

--- test/TauSigma/Statistics/AllanSpec.hs
+++ test/TauSigma/Statistics/AllanSpec.hs
@@ -1,22 +1,13 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module TauSigma.Statistics.AllanSpec where
 
 import Data.IntMap.Lazy (IntMap)
-import qualified Data.IntMap.Lazy as IntMap
-import Data.Tagged
-import Data.Vector (Vector)
 
 import Pipes
-import qualified Pipes.Prelude as P
-
-import System.Random.MWC.Monad (Rand, runWithCreate)
-
-import Text.Printf
 
 import TauSigma.Statistics.Allan (adevs)
-import TauSigma.Statistics.Theo1 (theoBRdevs)
+import TauSigma.Statistics.SlopeTest
 
 import TauSigma.Util.Pipes.Noise
   ( TimeData
@@ -33,28 +24,8 @@ import TauSigma.Util.Vector (takeVector)
 import Test.Hspec
 
 
-type Statistic = Vector Double -> IntMap Double
-type Slope = Double
-type Error = Double
-
-data TestCase
-  = TestCase { name        :: String
-             , description :: String
-             , samples     :: Int
-             , taus        :: (Int, Int)
-             , expected    :: Slope
-             , tolerance   :: Error
-             , statistic   :: Statistic
-             , noise       :: Producer (TimeData Double) (Rand IO) ()
-             }
-
 spec :: Spec
-spec = do
-  adevSpec
-  theoBRSpec
-
-adevSpec :: Spec
-adevSpec = describe "adev" (mapM_ slopeTest adevCases)
+spec = describe "adev" (mapM_ slopeTest adevCases)
 
 adevCases :: [TestCase]
 adevCases =
@@ -110,93 +81,3 @@ adevCases =
         wfm = whiteFrequency 1.0 >-> toPhase
         ffm = flickerFrequency (octaves sampleSize) 1.0 >-> toPhase
         rwfm = randomWalkFrequency 1.0  >-> toPhase
-
-theoBRSpec :: Spec
-theoBRSpec = describe "theoBR" (mapM_ slopeTest theoBRCases)
-
-theoBRCases :: [TestCase]
-theoBRCases =
-  [ TestCase { name        = "wfm"
-             , description = "Slope of white frequency noise ~ -0.5"
-             , samples     = sampleSize
-             , taus        = (30, 50)
-             , expected    = (-0.5)
-             , tolerance   = 0.1
-             , statistic   = theoBRdevs 1
-             , noise       = wfm
-             }
-  , TestCase { name        = "ffm"
-             , description = "Slope of flicker frequency noise ~ 0.0"
-             , samples     = sampleSize
-             , taus        = (30, 50)
-             , expected    = (0.0)
-             , tolerance   = 0.25
-             , statistic   = theoBRdevs 1
-             , noise       = ffm
-             }
-  , TestCase { name        = "rwfm"
-             , description = "Slope of random walk frequency noise ~ 0.5"
-             , samples     = sampleSize
-             , taus        = (30, 50)
-             , expected    = (0.5)
-             , tolerance   = 0.05
-             , statistic   = theoBRdevs 1
-             , noise       = rwfm
-             }
-  ]
-  where sampleSize = 3500
-        wfm = whiteFrequency 1.0 >-> toPhase
-        ffm = flickerFrequency (octaves sampleSize) 1.0 >-> toPhase
-        rwfm = randomWalkFrequency 1.0  >-> toPhase
-
-
--- | Validate that the slope of the tau/sigma plot is more or less
--- what it should be for the given noise source.
---
--- Note that testing at sampling intervals that are either too low or
--- too high tends to produce larger divergences from the expected
--- value.  That's what the second argument (the range of intervals) is
--- for; you want to pick "middle" values.
---
--- Also note that this test is based on 'runWithCreate' and as such is not
--- random at all.  This is a feature, not a bug, IMHO.
---
-slopeTest :: TestCase -> Spec
-slopeTest TestCase {..} =
-  describe name $ do
-    it description $ do
-      input <- runWithCreate (takeVector samples (noise >-> P.map unTagged))
-      let graph = filterKeys taus (statistic input)
-      let failures = badSlopes expected tolerance graph
-      failures `shouldBe` []
-
-
-filterKeys :: (Int, Int) -> IntMap a -> IntMap a
-filterKeys (lo,hi) = IntMap.filterWithKey go
-  where go key _ = lo <= key && key <= hi 
-
-type Point = (Int, Double)
-
-badSlopes :: Slope -> Error -> IntMap Double -> [(Int, Int, Slope, Error)]
-badSlopes standard tolerance graph =
-  [ (x0, x1, round slope, round err)
-  | let points = IntMap.toList graph
-  , (p0@(x0, y0), p1@(x1, y1)) <- withSuccessors (,) points
-  , let slope = logSlope (fromIntegral x0, y0) (fromIntegral x1, y1)
-  , let err = abs (slope - standard)
-  , err > tolerance
-  ]
-  where round :: Double -> Double
-        round x = read (printf "%.5f" x)
-
-withSuccessors :: (a -> a -> r) -> [a] -> [r]
-withSuccessors _ [] = []
-withSuccessors f (a:as) = map (f a) as ++ withSuccessors f as
-
-logSlope :: (Double, Double) -> (Double, Double) -> Double
-logSlope (x0,y0) (x1,y1) = slope (log x0, log y0) (log x1, log y1)
-  where
-    slope :: (Double, Double) -> (Double, Double) -> Double
-    slope (x0,y0) (x1,y1) = (y1 - y0) / (x1 - x0)
-                
-
---
+++ test/TauSigma/Statistics/SlopeTest.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE RecordWildCards #-}
+
+-- | Utility to run tau-sigma slope tests.  
+module TauSigma.Statistics.SlopeTest
+       ( Statistic
+       , Slope
+       , Error
+       , TestCase(..)
+       , slopeTest
+       ) where
+
+import Data.IntMap.Lazy (IntMap)
+import qualified Data.IntMap.Lazy as IntMap
+
+import Data.Tagged
+import Data.Vector (Vector)
+
+import Pipes
+import qualified Pipes.Prelude as P
+
+import System.Random.MWC.Monad (Rand, runWithCreate)
+
+import Text.Printf
+
+import TauSigma.Util.Pipes.Noise (TimeData)
+import TauSigma.Util.Vector (takeVector)
+
+import Test.Hspec
+
+
+type Statistic = Vector Double -> IntMap Double
+type Slope = Double
+type Error = Double
+
+data TestCase
+  = TestCase { name        :: String
+             , description :: String
+             , samples     :: Int
+             , taus        :: (Int, Int)
+             , expected    :: Slope
+             , tolerance   :: Error
+             , statistic   :: Statistic
+             , noise       :: Producer (TimeData Double) (Rand IO) ()
+             }
+
+-- | Validate that the slope of the tau/sigma plot is more or less
+-- what it should be for the given noise source.
+--
+-- Note that testing at sampling intervals that are either too low or
+-- too high tends to produce larger divergences from the expected
+-- value.  That's what the second argument (the range of intervals) is
+-- for; you want to pick "middle" values.
+--
+-- Also note that this test is based on 'runWithCreate' and as such is not
+-- random at all.  This is a feature, not a bug, IMHO.
+--
+slopeTest :: TestCase -> Spec
+slopeTest TestCase {..} =
+  describe name $ do
+    it description $ do
+      input <- runWithCreate (takeVector samples (noise >-> P.map unTagged))
+      let graph = filterKeys taus (statistic input)
+      let failures = badSlopes expected tolerance graph
+      failures `shouldBe` []
+
+
+filterKeys :: (Int, Int) -> IntMap a -> IntMap a
+filterKeys (lo,hi) = IntMap.filterWithKey go
+  where go key _ = lo <= key && key <= hi 
+
+type Point = (Int, Double)
+
+badSlopes :: Slope -> Error -> IntMap Double -> [(Int, Int, Slope, Error)]
+badSlopes standard tolerance graph =
+  [ (x0, x1, round slope, round err)
+  | let points = IntMap.toList graph
+  , (p0@(x0, y0), p1@(x1, y1)) <- withSuccessors (,) points
+  , let slope = logSlope (fromIntegral x0, y0) (fromIntegral x1, y1)
+  , let err = abs (slope - standard)
+  , err > tolerance
+  ]
+  where round :: Double -> Double
+        round x = read (printf "%.5f" x)
+
+withSuccessors :: (a -> a -> r) -> [a] -> [r]
+withSuccessors _ [] = []
+withSuccessors f (a:as) = map (f a) as ++ withSuccessors f as
+
+logSlope :: (Double, Double) -> (Double, Double) -> Double
+logSlope (x0,y0) (x1,y1) = slope (log x0, log y0) (log x1, log y1)
+  where
+    slope :: (Double, Double) -> (Double, Double) -> Double
+    slope (x0,y0) (x1,y1) = (y1 - y0) / (x1 - x0)
+                
+
