0f233453a24e43346d3d3c6ef46c1c476a47df24
Luis Casillas <luis@casillas.org>
2016-01-02 17:17:01 -0800

    Add TOTVAR statistics to library, and unit test

---
+++ src/TauSigma/Statistics/Total.hs
@@ -0,0 +1,71 @@
+
+-- | Utility functions for the Total deviation family of functions.  See:
+--
+-- * Howe, D.A. and C. A. Greenhall.  1997. \"Total Variance: a
+--   Progress Report on a New Frequency Stability Characterization\."
+--   Proc. 1997 PTTI Mtg., pp. 39-48 (Dec. 1997).  Web:
+--   http://tf.nist.gov/general/pdf/1257.pdf
+-- * Handbook of Frequency Stability Analysis
+--
+module TauSigma.Statistics.Total
+       ( Tau0
+       , (!*)
+       , totvar
+       , totdev
+       , totvars
+       , totdevs
+       ) where
+
+import Data.IntMap.Lazy (IntMap)
+import Data.Vector.Generic (Vector, (!))
+import qualified Data.Vector.Generic as V
+
+import TauSigma.Statistics.Util
+
+
+-- | Extend a time error sequence by reflection around the ends.
+(!*) :: Num a => Vector v a => v a -> Int -> a
+{-# INLINE (!*) #-}
+xs !* i
+  | i < 0           = 2*(V.head xs) - xs!(-i)
+  | i < V.length xs = xs!i
+  | otherwise       = let j = V.length xs - (i `mod` V.length xs) - 2
+                      in 2*(V.last xs) - xs!j
+
+{-
+xs !* i
+  | i < 0 = 2*xs!0 - xs!(-i)
+  | V.length xs <= i = 2*(V.last xs) - xs!(V.length xs - i)
+  | otherwise = xs!i
+-}
+infixl 9 !*
+
+-- | TOTVAR estimator at one sampling interval.
+totvar :: (Fractional a, Vector v a) => Tau0 -> Int -> v a -> a
+totvar tau0 m xs = sumsq / fromIntegral divisor
+  where divisor = 2 * m^2 * tau0^2 * (V.length xs - 2)
+        sumsq = sumGen (V.length xs - 1) term
+          where term notI = (xs!*(i-m) - 2*xs!*i + xs!*(i+m))^2
+                  where i = notI+1
+
+-- | Overlapped estimator of Allan deviation at one sampling interval.
+totdev :: (Floating a, Vector v a) => Tau0 -> Int -> v a -> a
+totdev tau0 m xs = sqrt (totvar tau0 m xs)
+
+-- | Overlapped estimator of Allan variance at all sampling intervals.
+-- Note that this returns a lazy 'IntMap' whose thunks hold on to the
+-- input vector.  You're going to want to force the ones you want right
+-- away and discard the map!
+totvars :: (RealFrac a, Vector v a) => Tau0 -> v a -> IntMap a
+totvars tau0 xs = allTaus [1..maxTaus] (totvar tau0) xs
+  where maxTaus = V.length xs - 2
+                    
+-- | Overlapped estimator of Allan deviation at all sampling intervals.
+-- Note that this returns a lazy 'IntMap' whose thunks hold on to the
+-- input vector.  You're going to want to force the ones you want
+-- right away and discard the map!
+totdevs :: (RealFloat a, Vector v a) => Tau0 -> v a -> IntMap a
+totdevs tau0 xs = allTaus [1..maxTaus] (totdev tau0) xs
+  where maxTaus = V.length xs - 1
+
+
---
+++ test/TauSigma/Statistics/TotalSpec.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module TauSigma.Statistics.TotalSpec where
+
+import Data.IntMap.Lazy (IntMap)
+import qualified Data.Vector.Unboxed as U
+
+import Pipes
+
+import TauSigma.Statistics.Total (totdevs, (!*))
+import TauSigma.Statistics.SlopeTest
+
+import TauSigma.Util.Pipes.Noise
+  ( whitePhase
+  , flickerPhase
+  , whiteFrequency
+  , flickerFrequency
+  , randomWalkFrequency
+  , toPhase
+  , octaves
+  )
+
+import Test.Hspec (Spec, describe, it)
+import Test.QuickCheck (property)
+
+
+spec :: Spec
+spec = do
+  describe "reflection" $
+    it "A reflected time sequence is defined correctly at all of its range" $
+      property prop_reflection
+      
+  describe "totdev" (mapM_ slopeTest totdevCases)
+
+prop_reflection :: Int -> Bool
+prop_reflection size = map (xs!*) xs' == xs'
+  where xs :: U.Vector Int
+        xs = U.generate size id
+        xs' :: [Int]
+        xs' = [2 - size .. 2*size - 3]
+
+
+totdevCases :: [TestCase]
+totdevCases =
+  [ TestCase { name        = "wpm"
+             , description = "Slope of white phase noise ~ -1.0"
+             , samples     = sampleSize
+             , taus        = (10, 20)
+             , expected    = (-1.0)
+             , tolerance   = 0.05
+             , statistic   = totdevs 1
+             , noise       = wpm
+             }
+  , TestCase { name        = "fpm"
+             , description = "Slope of flicker phase noise ~ -1.0"
+             , samples     = sampleSize
+             , taus        = (10, 20)
+             , expected    = (-1.0)
+             , tolerance   = 0.18
+             , statistic   = totdevs 1
+             , noise       = fpm
+             }
+  , TestCase { name        = "wfm"
+             , description = "Slope of white frequency noise ~ -0.5"
+             , samples     = sampleSize
+             , taus        = (10, 20)
+             , expected    = (-0.5)
+             , tolerance   = 0.05
+             , statistic   = totdevs 1
+             , noise       = wfm
+             }
+  , TestCase { name        = "ffm"
+             , description = "Slope of flicker frequency noise ~ 0.0"
+             , samples     = sampleSize
+             , taus        = (10, 20)
+             , expected    = (0.0)
+             , tolerance   = 0.05
+             , statistic   = totdevs 1
+             , noise       = ffm
+             }
+  , TestCase { name        = "rwfm"
+             , description = "Slope of random walk frequency noise ~ 0.5"
+             , samples     = sampleSize
+             , taus        = (10, 20)
+             , expected    = (0.5)
+             , tolerance   = 0.05
+             , statistic   = totdevs 1
+             , noise       = rwfm
+             }
+  ]
+  where sampleSize = 200000
+        wpm = whitePhase 1.0
+        fpm = flickerPhase (octaves sampleSize) 1.0
+        wfm = whiteFrequency 1.0 >-> toPhase
+        ffm = flickerFrequency (octaves sampleSize) 1.0 >-> toPhase
+        rwfm = randomWalkFrequency 1.0  >-> toPhase
