f58c2ea51b2f99a936f5c2ea17eacf0f7c338d42
Luis Casillas <luis.casillas@progressfin.com>
2016-01-15 12:16:10 -0800

    Tweak and update the examples.

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -6,22 +6,29 @@
 set -xe
 cd `dirname $0`/..
 
-VALUE=0.575
 TAUS=9
+
+# Noise levels for each type of noise, to produce a chart with the
+# correct range to fit the chart and produce the canonical slope.
+wpm=0.57
+fpm=0.4
+wfm=0.575
+ffm=0.4
+rwfm=0.4
 
 for noise in wpm fpm wfm ffm rwfm
 do
-    tau-sigma noise --${noise} ${VALUE} \
+    tau-sigma noise --${noise} ${!noise} \
         | tau-sigma chart \
                     --label "time error" \
                     --out images/${noise}_phase.svg
 
-    tau-sigma noise --${noise} ${VALUE} --frequency \
+    tau-sigma noise --${noise} ${!noise} --frequency \
         | tau-sigma chart \
                     --label "frequency error" \
                     --out images/${noise}_frequency.svg
     
-    tau-sigma noise --${noise} ${VALUE} \
+    tau-sigma noise --${noise} ${!noise} \
         | tau-sigma adev --tau0 1 --max-tau ${TAUS} \
         | tau-sigma loglog \
                     --label "ADEV" \
