6e6555eefa5899fab31f2413c816bf3c976414bb
Luis Casillas <luis.casillas@progressfin.com>
2016-01-15 16:54:13 -0800

    Fix mistake in IERS PNG example switchover, and bump size up.

--- scripts/iers.sh
+++ scripts/iers.sh
@@ -12,7 +12,7 @@ cd `dirname $0`/..
 INPUT=example-data/eopc04_08.1962-01-01_2015-12-15.csv
 
 TMP=`mktemp -d -t tmp`
-CONVERT="convert -density 1200 -resize 600x600"
+CONVERT="convert -density 1200 -resize 800x800"
 
 xsv select -d ';' LOD ${INPUT} \
     | xsv fmt -d ';' \
@@ -24,4 +24,4 @@ xsv select -d ';' LOD ${INPUT} \
                 --output-denominator 86400 \
     | tau-sigma totdev --tau0 86400 --max-tau 10000 \
     | tau-sigma loglog --label "totdev" --out ${TMP}/earth-1960-2015.svg
-$CONVERT ${TMP}/earth-1960-2015.svg images/earth-1960-2015.svg
+$CONVERT ${TMP}/earth-1960-2015.svg images/earth-1960-2015.png
