6bfa8f0e6d3245108d7c15398a77dc313a2354f5
Luis Casillas <luis@casillas.org>
2016-01-15 22:36:01 -0800

    Kill off variables no longer needed.

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -12,14 +12,6 @@ cd `dirname $0`/..
 TMP=`mktemp -d -t tmp`
 TAUS=10
 
-# Noise levels for each type of noise, to produce a chart with the
-# correct range to fit the chart and produce the canonical slope.
-wpm=0.525
-fpm=0.41
-wfm=0.575
-ffm=0.4
-rwfm=0.4
-
 export CONVERT="convert -density 1200 -resize 600x600"
 
 parallel "tau-sigma noise --{} 1.0 > ${TMP}/{}.csv" \
